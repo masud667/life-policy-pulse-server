7e033cad8a67f4b6aee475ffe56c8524b20990bc
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-07-24 16:44:19 +0600

    added jwt varify token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
@@ -8,19 +9,41 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdz9cop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+app.use(cookieParser());
+
+// verify Token middleware
+const verifyToken =(req, res, next) =>{
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  jwt.verify(token, process.env.KEY_SECRET, (err, decoded) => {
+    if (err) return res.status(403).send({ message: "Forbidden" });
+    req.user = decoded;
+    next();
+
+  //    const userEmail = req.user.email;
+  // const queryEmail = req.query.email;
+  // if (userEmail !== queryEmail) {
+  //   return res.status(403).send({ message: "Forbidden" });
+  // }
+
+  
+  });
+}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdz9cop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // MongoDB connection
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-
 
 async function run() {
   try {
@@ -28,14 +51,29 @@ async function run() {
     const db = client.db("LifePolicyPulse");
     const usersCollection = db.collection("users");
 
-
     // jwt token related
-app.post('/jwt',async (req, res) =>{
-  const {email} = req.body;
-  const user = {email}
-  const token = jwt.sign(user, process.env.KEY_SECRET, {expiresIn: '1h'})
-  res.send({token})
-})
+    app.post("/jwt", async (req, res) => {
+      const { email } = req.body;
+      const user = { email };
+      const token = jwt.sign(user, process.env.KEY_SECRET, { expiresIn: "1d" });
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+      });
+      res.send({ token });
+    });
+
+
+//logout 
+    app.post('/logout', (req, res) => {
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: false,
+  
+  });
+  res.send({ message: 'Logged out successfully' });
+});
 
     // Routes
     app.get("/", (req, res) => {
@@ -52,7 +90,6 @@ app.post('/jwt',async (req, res) =>{
       const result = await usersCollection.insertOne(newUser);
       res.send(result);
     });
-
   } catch (error) {
     console.error(error);
   }
